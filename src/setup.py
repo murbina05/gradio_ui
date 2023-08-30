2213a5539f524342a0d51fae943f822152acd010
murbina05 <109252973+murbina05@users.noreply.github.com>
2023-08-30 14:04:06 -0700

    Add files via upload

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+NAME = "gradio-ui"
+VERSION = "1.0"
+
+AUTHOR = "Maxwell Urbina"
+EMAIL= "maurbina@ucsd.edu"
+
+if __name__ == "__main__":
+    setup(version=VERSION,
+          name= NAME,
+          author=AUTHOR,
+          author_email=EMAIL,
+
+          )
